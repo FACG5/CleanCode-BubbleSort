d742e431db2eada89d13d8127cc35e3cdf04b2c5
knanahassouna1 <knanahassouna2@gmail.com>
2018-08-14 16:07:34 +0300

    comments

---
+++ Bubble-sort.js
@@ -0,0 +1,32 @@
+/*
+*   
+*
+*
+* 
+*/
+
+// swap item has index i with item has index j
+function swap(array, firstItem, secandItem) {
+    // temp to store frstItem temprory  
+    var temp = array[firstItem];
+    array[firstItem] = array[secandItem];
+    array[secandItem] = temp;
+}
+
+// takse a  array and sort it ascending
+function bubble(arr) {
+    //length of array
+    var len = arr.length,
+        i, j, stop;
+
+    for (i = 0; i < len; i++) {
+        for (j = 0, stop = len - i; j < stop; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    return arr;
+}
+console.log(bubble([3, 0, 2, 5, -1, 4, 1]));
